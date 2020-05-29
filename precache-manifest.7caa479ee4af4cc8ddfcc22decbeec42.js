ef39f6fc2b31efced2d6e670260c4c693285615d
Mohan Balasubramani <mohan_krish18@yahoo.in>
2020-05-29 18:31:43 +0530

    Add files via upload

---
+++ precache-manifest.7caa479ee4af4cc8ddfcc22decbeec42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0639444ed24c866747e2612cca677a95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a3f61b37ebf12731cce",
+    "url": "/static/css/main.faf994b9.chunk.css"
+  },
+  {
+    "revision": "7bf8c62dabdd9359d0b2",
+    "url": "/static/js/2.f79cb853.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f79cb853.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a3f61b37ebf12731cce",
+    "url": "/static/js/main.45a57778.chunk.js"
+  },
+  {
+    "revision": "491f77872c7497d97f0e",
+    "url": "/static/js/runtime-main.79fdd551.js"
+  }
+]);
